cb7d095dbbbe68523a64a930340d09d6106e6ac1
Roya <roya.shahroudi20@gmail.com>
2024-09-20 19:02:47 +0330

    feat: implement list of movies component

---
+++ src/app/_components/movieList.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import StarIcon from '@mui/icons-material/Star';
+import Link from "next/link";
+
+const API_KEY = process.env.API_KEY
+
+const MovieList = async () => {
+
+    const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
+    if (!res.ok) {
+        throw new Error("Failed to fetch data")
+    }
+    const data = await res.json();
+    const menuItem = data.results;
+
+    return (
+        <div className="max-w-[1640px] py-12 grid lg:grid-cols-3 grid-cols-2 gap-6">
+            {menuItem.map(({id, title, poster_path, release_date, vote_average}: {
+                id: number,
+                title: string,
+                poster_path: string,
+                release_date: number,
+                vote_average: number
+            }, index: number) => {
+
+                const date = new Date(release_date);
+                const formattedDate = date.toLocaleString("en-US", {month: "short", year: "numeric"});
+
+                return (
+                    <Link href={`/movies/${id}`}>
+                        <div key={index} className="rounded-xl relative">
+                            <div
+                                className="absolute bottom-[-1px] left-0 w-full px-2 h-1/3 bg-gray-500/50 backdrop-blur-sm rounded-xl text-white">
+                                <div className="flex items-center justify-between h-full">
+                                    <div className="px-2 w-2/3">
+                                        <p className="md:text-lg font-semibold truncate">{title}</p>
+                                        <p className="text-sm font-light text-gray-300">{formattedDate}</p>
+                                    </div>
+                                    <div className="flex items-center">
+                                        <p className="text-sm font-lght text-gray-100 mr-1">
+                                            {Number((vote_average).toFixed(1))}
+                                        </p>
+                                        <StarIcon sx={{fontSize: 18}}/>
+                                    </div>
+                                </div>
+                            </div>
+                            <img
+                                className="max-h-[160px]  md:max-h-[200px] w-full object-cover rounded-xl"
+                                src={`https://image.tmdb.org/t/p/original${poster_path}`}
+                                alt="/"
+                            />
+                        </div>
+                    </Link>
+                );
+            })}
+        </div>
+    );
+};
+
+export default MovieList;
